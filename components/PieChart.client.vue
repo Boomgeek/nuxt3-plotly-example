a228102597941790b41645ce8fa641dfb5a1fd18
Boomgeek <supanut@superdev.tech>
2023-07-27 11:35:51 +0700

    1 file modified

--- components/PieChart.client.vue
+++ components/PieChart.client.vue
@@ -15,100 +15,89 @@
 
 <script setup>
 import { reactive } from 'vue';
+const data = [
+  [
+    {
+      values: [19, 26, 55],
+      labels: ['Residential', 'Non-Residential', 'Utility'],
+      type: 'pie',
+    },
+  ],
+  [
+    {
+      values: [16, 15, 12, 6, 5, 4, 42],
+      labels: [
+        'US',
+        'China',
+        'European Union',
+        'Russian Federation',
+        'Brazil',
+        'India',
+        'Rest of World',
+      ],
+      domain: { column: 0 },
+      name: 'GHG Emissions',
+      hoverinfo: 'label+percent+name',
+      hole: 0.4,
+      type: 'pie',
+    },
+    {
+      values: [27, 11, 25, 8, 1, 3, 25],
+      labels: [
+        'US',
+        'China',
+        'European Union',
+        'Russian Federation',
+        'Brazil',
+        'India',
+        'Rest of World',
+      ],
+      text: 'CO2',
+      textposition: 'inside',
+      domain: { column: 1 },
+      name: 'CO2 Emissions',
+      hoverinfo: 'label+percent+name',
+      hole: 0.4,
+      type: 'pie',
+    },
+  ],
+];
 
-const data0 = [
+const layout = [
   {
-    values: [19, 26, 55],
-    labels: ['Residential', 'Non-Residential', 'Utility'],
-    type: 'pie',
+    title: '[layout0] Pie Chart on `PieChart.client.vue`',
+  },
+  {
+    title: '[layout1] Pie Chart on `PieChart.client.vue`',
   },
 ];
 
-const data1 = [
-  {
-    values: [16, 15, 12, 6, 5, 4, 42],
-    labels: [
-      'US',
-      'China',
-      'European Union',
-      'Russian Federation',
-      'Brazil',
-      'India',
-      'Rest of World',
-    ],
-    domain: { column: 0 },
-    name: 'GHG Emissions',
-    hoverinfo: 'label+percent+name',
-    hole: 0.4,
-    type: 'pie',
-  },
-  {
-    values: [27, 11, 25, 8, 1, 3, 25],
-    labels: [
-      'US',
-      'China',
-      'European Union',
-      'Russian Federation',
-      'Brazil',
-      'India',
-      'Rest of World',
-    ],
-    text: 'CO2',
-    textposition: 'inside',
-    domain: { column: 1 },
-    name: 'CO2 Emissions',
-    hoverinfo: 'label+percent+name',
-    hole: 0.4,
-    type: 'pie',
-  },
+const config = [
+  { scrollZoom: true, displayModeBar: true },
+  { scrollZoom: false, displayModeBar: false },
 ];
-
-const layout0 = {
-  title: '[layout0] Pie Chart on `PieChart.client.vue`',
-};
-const layout1 = {
-  title: '[layout1] Pie Chart on `PieChart.client.vue`',
-};
-
-const config0 = { scrollZoom: true, displayModeBar: true };
-const config1 = { scrollZoom: false, displayModeBar: false };
 
 const pieChart = reactive({
   changeDataNo: 1,
-  data: data0,
+  data: data[0],
   changeConfigNo: 1,
-  config: config0,
+  config: config[0],
   changeLayoutNo: 1,
-  layout: layout0,
+  layout: layout[0],
 });
 
 function changeData() {
-  if (pieChart.changeDataNo === 1) {
-    pieChart.data = data1;
-    pieChart.changeDataNo = 0;
-  } else {
-    pieChart.data = data0;
-    pieChart.changeDataNo = 1;
-  }
+  pieChart.data = data[pieChart.changeDataNo];
+  pieChart.changeDataNo = pieChart.changeDataNo == 1 ? 0 : 1;
 }
 
 function changeLayout() {
-  if (pieChart.changeLayoutNo === 1) {
-    pieChart.layout = layout1;
-    pieChart.changeLayoutNo = 0;
-  } else {
-    pieChart.layout = layout0;
-    pieChart.changeLayoutNo = 1;
-  }
+  pieChart.layout = layout[pieChart.changeLayoutNo];
+  pieChart.changeLayoutNo = pieChart.changeLayoutNo == 1 ? 0 : 1;
 }
 
 function changeConfig() {
-  if (pieChart.changeConfigNo === 1) {
-    pieChart.config = config1;
-    pieChart.changeConfigNo = 0;
-  } else {
-    pieChart.config = config0;
-    pieChart.changeConfigNo = 1;
-  }
+  pieChart.config = config[pieChart.changeConfigNo];
+  pieChart.changeConfigNo = pieChart.changeConfigNo == 1 ? 0 : 1;
 }
 </script>
